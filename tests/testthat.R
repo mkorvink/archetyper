8e0a265817785456126e2ea6426966cc80b5a36a
Michael Korvink <michael_korvink@premierinc.com>
2021-02-20 16:46:34 -0500

    adding citation and unit tests to validate project name arguement.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,36 @@
 library(testthat)
 library(archityper)
-
 test_check("archityper")
+
+is_valid_project_name <- function(project_name) {
+  return(stringr::str_detect(project_name, "[\\\\|\\/|:|\\<|\\>|\\|]", negate = T) & stringr::str_starts(project_name, "\\.", negate = T))
+}
+
+test_that("Testing happy path project name", {
+  expect_equal(is_valid_project_name("Hello"), T)
+})
+
+test_that("Project names can include a period", {
+  expect_equal(is_valid_project_name("22.891"), T)
+})
+
+test_that("Project names cannot include a /", {
+  expect_equal(is_valid_project_name("my/project"), F)
+})
+
+test_that("Project names cannot include a |", {
+  expect_equal(is_valid_project_name("my|project"), F)
+})
+
+test_that("Project names cannot include a >", {
+  expect_equal(is_valid_project_name("my>project"), F)
+})
+
+test_that("Project names cannot include a <", {
+  expect_equal(is_valid_project_name("my < project"), F)
+})
+
+test_that("Project names cannot include a :", {
+  expect_equal(is_valid_project_name("my : project"), F)
+})
+
